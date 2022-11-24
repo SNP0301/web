2b1398b5066cd6ce2327a1bbd5ec57c24162aefb
Seung-Gi0301 <studiosnp0301@gmail.com>
2022-11-24 13:31:04 +0900

    [SLN] get attribute, excel read DONE

--- selenium/open_google.py
+++
@@ -1,4 +0,0 @@
-from selenium import webdriver ### import webdriver
-driver = webdriver.Chrome()
-url = 'https://www.google.com'
-driver.get(url)
--- selenium/search_school.py
+++ selenium/search_school.py
@@ -4,30 +4,49 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+import pandas as pd
+import openpyxl as op
 
-
+## 00. Setup Environment
 options = Options()
 options.add_experimental_option('detach',True)
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://schoolzone.emac.kr/gis/gis.do")
 
+school_list = pd.read_excel('/Users/snp/Documents/schools.xlsx')
+
+w_book = op.load_workbook('/Users/snp/Documents/schools.xlsx')
+w_sheet = w_book.active
+print(w_sheet.cell(row=1,column=2).value)
+print(w_sheet.cell(row=2,column=3).value)
+print(w_sheet.cell(row=3,column=4).value)
+
 school_name = driver.find_element(By.ID,'searchText')
 school_name.send_keys('살레시오고등학교' + Keys.ENTER)
-time.sleep(0.5)
-driver.find_element(By.XPATH,'/html/body/div[1]/div[11]/div[1]/p/input').send_keys(Keys.ENTER)
-driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
 
-time.sleep(1)
+#time.sleep(0.5) ## wait for 500m
+#driver.find_element(By.XPATH,'/html/body/div[1]/div[11]/div[1]/p/input').send_keys(Keys.ENTER)
+#driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
 
-elem = driver.find_element(By.CSS_SELECTOR,'body > div:nth-child(4) > div:nth-child(3) > ul.address_list')
+## 01. Move on search frame and get attribute: address of x and y
+e_frame = driver.find_element(By.XPATH,'//*[@id="searchIframe"]')
+driver.switch_to.frame(e_frame)
+
+elem = driver.find_element(By.XPATH,'/html/body/div[4]/div[1]/ul[1]')
 x_addr = elem.get_attribute('x')
 y_addr = elem.get_attribute('y')
-x = "126.892490626667"
-y = "35.2166294135392"
-cmd_dup = "parent.fn_getSchoolArea("
-cmd_dup = cmd_dup + x + ',' + y + ',\'middleSchoolArea\',\'vworld\');'
-cmd_dup + ','
-print(cmd_dup)
 
+## 02. Search school area using address
+cmd_dup = "parent.fn_getSchoolArea(" + x_addr + ',' + y_addr + ',\'middleSchoolArea\',\'vworld\');'
 driver.execute_script(cmd_dup)
+driver.switch_to.default_content()
+
+## 03. Move on search frame and extract school area text
+driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
+i_frame = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/iframe')
+driver.switch_to.frame(i_frame)
+
+area_name = driver.find_element(By.XPATH,'//*[@id="middleSchoolArea"]/div/p').text
+print(area_name)
+driver.switch_to.default_content()
