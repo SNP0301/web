9558ca5be4db87e2e861e44b37d026cfa1d7ab09
Seung-Gi0301 <studiosnp0301@gmail.com>
2022-11-24 17:23:17 +0900

    [SLN] School Searching Project DONE

--- selenium/search_school.py
+++ selenium/search_school.py
@@ -18,35 +18,58 @@ school_list = pd.read_excel('/Users/snp/Documents/schools.xlsx')
 
 w_book = op.load_workbook('/Users/snp/Documents/schools.xlsx')
 w_sheet = w_book.active
-print(w_sheet.cell(row=1,column=2).value)
-print(w_sheet.cell(row=2,column=3).value)
-print(w_sheet.cell(row=3,column=4).value)
 
-school_name = driver.find_element(By.ID,'searchText')
-school_name.send_keys('살레시오고등학교' + Keys.ENTER)
+result_book = op.load_workbook('/Users/snp/Documents/result.xlsx')
+result_sheet = result_book.active
+
+i = 2
 
 #time.sleep(0.5) ## wait for 500m
 #driver.find_element(By.XPATH,'/html/body/div[1]/div[11]/div[1]/p/input').send_keys(Keys.ENTER)
 #driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
+for i in range(1001, 2800):
+    search_name = w_sheet.cell(row=i, column=3).value
+    print(search_name)
 
-## 01. Move on search frame and get attribute: address of x and y
-e_frame = driver.find_element(By.XPATH,'//*[@id="searchIframe"]')
-driver.switch_to.frame(e_frame)
 
-elem = driver.find_element(By.XPATH,'/html/body/div[4]/div[1]/ul[1]')
-x_addr = elem.get_attribute('x')
-y_addr = elem.get_attribute('y')
+    school_name = driver.find_element(By.ID,'searchText')
+    school_name.send_keys(search_name + Keys.ENTER)
 
-## 02. Search school area using address
-cmd_dup = "parent.fn_getSchoolArea(" + x_addr + ',' + y_addr + ',\'middleSchoolArea\',\'vworld\');'
-driver.execute_script(cmd_dup)
-driver.switch_to.default_content()
+    ##driver.find_element(By.ID,'searchText').clear()
 
-## 03. Move on search frame and extract school area text
-driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
-i_frame = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/iframe')
-driver.switch_to.frame(i_frame)
+    e_frame = driver.find_element(By.XPATH,'//*[@id="searchIframe"]')
+    driver.switch_to.frame(e_frame)
 
-area_name = driver.find_element(By.XPATH,'//*[@id="middleSchoolArea"]/div/p').text
-print(area_name)
-driver.switch_to.default_content()
+    try: 
+        elem = driver.find_element(By.XPATH,'/html/body/div[4]/div[1]/ul[1]')
+        x_addr = elem.get_attribute('x')
+        y_addr = elem.get_attribute('y')
+
+
+        cmd_dup = "parent.fn_getSchoolArea(" + x_addr + ',' + y_addr + ',\'middleSchoolArea\',\'vworld\');'
+        driver.execute_script(cmd_dup)
+        driver.switch_to.default_content()
+
+    ## 03. Move on search frame and extract school area text
+        driver.find_element(By.XPATH,'/html/body/div[1]/div[10]/div[1]/p/input').send_keys(Keys.ENTER)
+        i_frame = driver.find_element(By.XPATH,'/html/body/div[1]/div[2]/div[1]/div[2]/div/div[2]/iframe')
+        driver.switch_to.frame(i_frame)
+
+        area_name = driver.find_element(By.XPATH,'//*[@id="middleSchoolArea"]/div/p').text
+
+        result_sheet.cell(row=i, column=1).value = search_name
+        result_sheet.cell(row=i, column=2).value = area_name
+        result_book.save('/Users/snp/Documents/result.xlsx')
+
+        print(area_name)
+
+    except:
+        result_sheet.cell(row=i, column=1).value = search_name
+        result_sheet.cell(row=i, column=2).value = "not exists"
+        result_book.save('/Users/snp/Documents/result.xlsx')
+        print('not exists')
+       
+    driver.switch_to.default_content()
+    school_name.send_keys(Keys.COMMAND + 'a')
+    school_name.send_keys(Keys.DELETE)
+    time.sleep(0.1)
