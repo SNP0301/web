12d943004717df692d374a3763a70526f19a1155
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-07 17:49:18 +0900

    Timeout error on ln 44 and 78

--- selenium/middle_school_io.py
+++ selenium/middle_school_io.py
@@ -24,20 +24,21 @@ driver.get("https://www.schoolinfo.go.kr/ei/ss/pneiss_a03_s0.do#")
 parent_window = driver.current_window_handle
 
 ### 1. 검색창을 클릭
-for i in range(67, 3279):
-    time.sleep(0.3)
+for i in range(459, 3279):
     search_box = driver.find_element(By.XPATH, '//*[@id="searchWord"]')
     search_box.click()
 
     ### 2. Input.txt에서 검색어를 따와서 입력
     target_school_name = output_sheet.cell(row=i, column = 14).value
+    search_box.click()
     search_box.send_keys(target_school_name)
-    time.sleep(1)
+    time.sleep(0.5)
     ##search_box.send_keys(Keys.ENTER)
 
 
     ### 3. 엔터 입력
     school_name_title = '//*[@title=\"' + output_sheet.cell(row=i, column = 7).value + '\"]'
+    time.sleep(1)
    ### print(school_name_title)
     WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,school_name_title)))
     driver.find_element(By.XPATH,school_name_title).click()
@@ -53,7 +54,7 @@ for i in range(67, 3279):
     ## passed
 
     ### 6. 공시정보 옆 년도칸 클릭, 22년 선택
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="gsYear"]')))
+    WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH,'//*[@id="gsYear"]')))
     select = Select(driver.find_element(By.XPATH, '//*[@id="gsYear"]'))
     select.select_by_value('2022')
 
@@ -74,9 +75,8 @@ for i in range(67, 3279):
     ##남자
     td_arr = [1,2,3,4,5,6,7,9,10,12,14,15,16]
 
-    WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH,'/html/body/div/div[3]/div[4]/div[3]/div[1]/div[2]/div/div[1]/table/tbody/tr[1]/td[1]')))
+    WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH,'/html/body/div/div[3]/div[4]/div[3]/div[1]/div[2]/div/div[1]/table/tbody/tr[1]/td[1]')))
 
-    
     k = 15
     for j in range(0,13):
         xpath_parameter_male = '/html/body/div/div[3]/div[4]/div[3]/div[1]/div[2]/div/div[1]/table/tbody/tr[1]/td[' + str(td_arr[j]) + ']'
@@ -89,15 +89,14 @@ for i in range(67, 3279):
         k = k + 1
 
     output_book.save('/Users/snp/web/Selenium/output.xlsx')
-    time.sleep(0.5)
 
     driver.close() ### 자녀 창 작업 종료
     driver.switch_to.window(parent_window) ## 부모창으로 전환   
-    time.sleep(1)
+    time.sleep(0.5)
 
     search_box.click()
     time.sleep(0.5)
     search_box.send_keys(Keys.COMMAND + 'A')
-    time.sleep(0.5)
+    time.sleep(0.7)
     search_box.send_keys(Keys.DELETE)
-    time.sleep(2)
+    time.sleep(0.7)
