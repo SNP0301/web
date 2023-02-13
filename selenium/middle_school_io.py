7a1e87412ec149df3147c9d9daf79f027626d306
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-02-13 17:50:05 +0900

    ~1479

--- selenium/middle_school_io.py
+++ selenium/middle_school_io.py
@@ -32,7 +32,7 @@ driver.get("https://www.schoolinfo.go.kr/ei/ss/pneiss_a03_s0.do#")
 parent_window = driver.current_window_handle
 
 ### 1. 검색창을 클릭
-for i in range(1126, 1500):
+for i in range(1472, 1500):
     time.sleep(1)
     search_box = driver.find_element(By.XPATH, '//*[@id="searchWord"]')
     search_box.click()
