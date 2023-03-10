2f269d65615b3b6704786cc9df4bdc304dc41a05
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-10 14:11:29 +0900

    Ch 5 Done

--- JS/clock.js
+++ JS/clock.js
@@ -1,9 +1,10 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
 
+
+function getClock() {
+    const date = new Date()
+    clock.innerText = `${String(date.getHours()).padStart(2,"0")}:${String(date.getMinutes()).padStart(2,"0")}:${String(date.getSeconds()).padStart(2,"0")}`;
 }
-
-setInterval(sayHello, 5000);
-setTimeout(sayHello, 5000);
+getClock()
+setInterval(getClock, 1000);
