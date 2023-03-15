c1b1a2b03e7220b9f321a26e7870b0f5148df2ee
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-15 17:02:11 +0900

    useEffect done

--- React/my-first-react/src/App.js
+++ React/my-first-react/src/App.js
@@ -2,15 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
 
   useEffect(() => {
     console.log("CALL THE API");
   }, []);
 
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
