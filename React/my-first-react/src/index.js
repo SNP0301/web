0e1eba7c7b73a1718897b97218f6528ea466cebc
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-15 16:16:09 +0900

    [SOLVED] - console.log is called once at a time

--- React/my-first-react/src/index.js
+++ React/my-first-react/src/index.js
@@ -4,8 +4,4 @@ import App from "./App";
 /// import "./styles.css"; /// CSS는 Proptypes와 다르게 모든 component의 색을 바꾸기 때문에 좋지 않다.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
