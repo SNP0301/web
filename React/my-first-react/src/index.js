f8ca43206705f2476ca435b0ea626e9cacf18fc6
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-03-15 16:14:28 +0900

    console.log is called twice at a time

--- React/my-first-react/src/index.js
+++ React/my-first-react/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+/// import "./styles.css"; /// CSS는 Proptypes와 다르게 모든 component의 색을 바꾸기 때문에 좋지 않다.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
