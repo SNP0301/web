df1fee32e4650beb33925c6cb29589ff81331acb
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-14 11:48:49 +0900

    api call done

--- React/react-for-nft/weather-app/src/App.js
+++ React/react-for-nft/weather-app/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [allData, setAllData] = useState({
+    city: "atlanta",
+    country: "",
+    temperature: "",
+  });
+
+  useEffect(() => {
+    // we add what we want to happen after rending
+    // --> fetch the DB
+    fetchData();
+  }, []);
+
+  const fetchData = async (city) => {
+    const APIKEY = "6c2bb102977fb085dce057619ea59995";
+    const result = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${"atlanta"}&appid=${APIKEY}`
+    );
+    await setAllData({
+      city: result,
+      country: result.data.sys.country,
+      temperature: result.data.main.temp,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App">{console.log("keep focus", allData.country)}</div>
   );
 }
 
